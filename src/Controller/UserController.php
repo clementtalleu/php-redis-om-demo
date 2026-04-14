fb3364b9b10640a1e8227c28517be1508db35a11
Eva CHEN <eva.chen@les-tilleuls.coop>
2026-04-14 17:09:07 +0200

    update

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -60,4 +60,5 @@ class UserController extends AbstractController{
     public function show(User $user): Response{
         return $this->render('admin/user/show.html.twig', ['user' => $user]);
     }
+
 }
